e8b5b3e339e6a21fde221b9bb656576d727a04cc
suresh30042004 <mathavan033@gmail.com>
2025-04-30 08:03:36 +0530

    create migration

--- app/Models/product.php
+++ app/Models/product.php
@@ -2,9 +2,41 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'brand_id',
+        'name',
+        'slug',
+        'images',
+        'description',
+        'price',
+        'is_active',
+        'is_featured',
+        'in_stock',
+        'on_sale',
+    ];
+
+    protected $casts = [
+        'image' => 'array'
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(){
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function orderItem(){
+        return $this->hasMany(orderitem::class);
+    }
 }
+
--- database/migrations/2025_04_29_120512_create_orders_table.php
+++ database/migrations/2025_04_29_120512_create_orders_table.php
@@ -17,7 +17,11 @@ return new class extends Migration
             $table->decimal('grand_total',10,2)->nullable();
             $table->string('payment_method')->nullable();
             $table->string('payment_status')->nullable();
-            $table->enum('status',['new','processing','shipped','delivered','canceled'])->nullable()
+            $table->enum('status',['new','processing','shipped','delivered','canceled'])->default('new');
+            $table->string('currency')->nullable();
+            $table->decimal('shipping_amount ',10,2)->nullable();
+            $table->string('shipping_method')->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
         });
     }
